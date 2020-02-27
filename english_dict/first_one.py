44a925bc5f10655e6abc639b33ab854785c09f01
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2020-02-27 14:48:52 +0300

    Add search for acronyms

--- english_dict/first_one.py
+++ english_dict/first_one.py
@@ -9,6 +9,8 @@ def translate(w):
         return data[w]
     elif w.title() in data:
         return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys(), cutoff=0.8)):
         ur = input('Did you mean %s instead? Enter Y if yes, or N if No: ' % get_close_matches(w, data.keys())[0])
         if ur.upper() == "Y":
